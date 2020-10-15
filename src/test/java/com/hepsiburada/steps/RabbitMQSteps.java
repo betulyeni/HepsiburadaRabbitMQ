42461f7ec35d5bfcafcefaa302364f645f391a63
betul.yeni <Btly.2495>
2020-10-15 10:41:40 +0300

    RabbitMQ-Client

--- src/test/java/com/hepsiburada/steps/RabbitMQSteps.java
+++ src/test/java/com/hepsiburada/steps/RabbitMQSteps.java
@@ -1,57 +1,46 @@
 package com.hepsiburada.steps;
+
 import com.rabbitmq.client.*;
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+
 import java.nio.charset.StandardCharsets;
+
 import static org.junit.Assert.assertEquals;
 
 public class RabbitMQSteps {
-    private final static String QUEUE_NAME = "hello";
+    public final static String QUEUE_NAME = "hello";
     public String writeMessage;
     public String readMessage;
-    public Channel channel;
 
+    RabbitMQClient rabbitMQClient;
 
-    public void setUp() throws Exception {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
-        channel = connection.createChannel();
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
+    public RabbitMQSteps(RabbitMQClient rabbitMQClient) {
+        this.rabbitMQClient = rabbitMQClient;
     }
+
     @Given("^Create new Message \"([^\"]*)\"$")
     public void createNewMessage(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        setUp();
         writeMessage = arg0;
-        channel.basicPublish("", QUEUE_NAME, null, writeMessage.getBytes(StandardCharsets.UTF_8));
+        rabbitMQClient.publishBrand(writeMessage);
         System.out.println(" [x] Sent '" + writeMessage + "'");
-        //throw new PendingException();
+
     }
 
     @When("^Reading in created message$")
     public void readingInMessage() throws Exception {
-        setUp();
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            readMessage = new String(delivery.getBody(), "UTF-8");
-            System.out.println(" [x] Received '" + readMessage + "'");
-        };
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
-        });
+        rabbitMQClient.basicConsume();
 
     }
 
     @Then("^Check the message read with the typed message$")
     public void checkTheMessageReadWithTheTypedMessage() throws Exception {
-        Thread.sleep(2000);
+        Thread.sleep(10000);
         assertEquals(writeMessage, readMessage);
 
     }
-
 
 
 }
